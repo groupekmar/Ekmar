884ff9f424caec6055fa61ce6bb0f38946a6e6cc
Ekmar <groupekmar@gmail.com>
2024-09-24 02:12:47 +0300

    added animated hamburger menu

--- src/components/header/child/mobileMenu/MobileMenu.jsx
+++ src/components/header/child/mobileMenu/MobileMenu.jsx
@@ -1,49 +1,51 @@
 "use client";
 import "./mobileMenu.scss";
 import { useTranslations, useLocale } from "next-intl";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Link from "next/link";
-import HamburgerIcon from "/src/assets/header/hamburger.svg";
-import CloseIcon from "/src/assets/header/close.svg";
+import Hamburger from "hamburger-react";
 
 export default function MobileMenu() {
-  const [isActive, setIsActive] = useState(false);
+  const [isOpen, setOpen] = useState(false);
   const t = useTranslations("header.desktopMenu");
-  const locale = useLocale();  // Mevcut dili al
+  const locale = useLocale();
 
-  const toggleMenu = () => {
-    setIsActive((isActive) => !isActive);
+  const handleClick = () => {
     document.querySelector("body").classList.toggle("overlay-active");
+    setOpen((isOpen) => !isOpen);
   };
 
   return (
     <div className="lap-dn">
-      <div className="df aic jce">
-        <HamburgerIcon onClick={toggleMenu} />
-        {isActive && (
-          <div className="mobileMenu">
-            <div className="df aic jce">
-              <CloseIcon onClick={toggleMenu} />
-            </div>
-            <ul>
-              <li>
-                <Link onClick={toggleMenu} href={`/${locale}`}>{t("home")}</Link>
-              </li>
-              <li>
-                <Link onClick={toggleMenu} href={`/${locale}/#about`}>{t("about")}</Link>
-              </li>
-              <li>
-                <Link onClick={toggleMenu} href={`/${locale}/#products`}>{t("products")}</Link>
-              </li>
-              <li>
-                <Link onClick={toggleMenu} href={`/${locale}/#partners`}>{t("partners")}</Link>
-              </li>
-              <li>
-                <Link onClick={toggleMenu} href={`/${locale}/#contact`}>{t("contact")}</Link>
-              </li>
-            </ul>
-          </div>
-        )}
+      <Hamburger toggled={isOpen} toggle={handleClick} color="#ffffff" />
+      <div className={`mobileMenu ${isOpen ? "active" : ""}`}>
+        <ul>
+          <li>
+            <Link onClick={handleClick} href={`/${locale}`}>
+              {t("home")}
+            </Link>
+          </li>
+          <li>
+            <Link onClick={handleClick} href={`/${locale}/#about`}>
+              {t("about")}
+            </Link>
+          </li>
+          <li>
+            <Link onClick={handleClick} href={`/${locale}/#products`}>
+              {t("products")}
+            </Link>
+          </li>
+          <li>
+            <Link onClick={handleClick} href={`/${locale}/#partners`}>
+              {t("partners")}
+            </Link>
+          </li>
+          <li>
+            <Link onClick={handleClick} href={`/${locale}/#contact`}>
+              {t("contact")}
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
